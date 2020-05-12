d31a7b9e901c578ead91e440708906e63061e472
Max S-T <maxstantay@gmail.com>
2020-05-12 17:50:21 +0000

    readme update and made it work better

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 	let rolled;
 	let col1roll;
 	let col2roll;
-
+	let dataUrl = 'https://raw.githubusercontent.com/manfromth3m0oN/dice/master/test.json'
 	onMount(async () => {
 		getJson().then(data => {
 			rawData = data
@@ -14,7 +14,7 @@
 	})
 
 	function getJson() {
-		return axios.get('https://raw.githubusercontent.com/manfromth3m0oN/dice/master/test.json')
+		return axios.get(dataUrl)
 			.then((response) => {
 				console.log(response.data)
 				return response.data
@@ -34,18 +34,22 @@
 		}
 		rolled = true
 	};
+	function clear() {
+		col1roll = -1;
+		col2roll = -1;
+	}
 </script>
 
 <main>
 	<button on:click={rollDie}>Roll the dice</button><br/>
-	{#if col1roll > 0}
+	{#if col1roll >= 0}
 		<p>Column 1:</p>{rawData['col1'][col1roll]}
 	{/if}
 
-	{#if col2roll > 0}
+	{#if col2roll >= 0}
 		<p>Column 2:</p>{rawData['col2'][col2roll]}
-	{/if}
-
+	{/if} <br/>
+	<button on:click={clear}>Clear the results</button>
 </main>
 
 <style>
