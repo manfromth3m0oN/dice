8aa7e8c8ff7a8bcb5fcb61fe5ac54ed07001cf4d
Max S-T <maxstantay@gmail.com>
2020-05-12 15:50:01 +0000

    intial commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	let roll = 0;
+	let listLength = 10;
+	function getRndInteger() {
+	roll = Math.floor(Math.random() * (listLength - 0 + 1) ) + 0;
+	}
+
+
+</script>
+
+<main>
+	The roll is {roll}
+	<button on:click={getRndInteger}>Roll the dice</button>
+</main>
+
+<style>
+
+</style>
