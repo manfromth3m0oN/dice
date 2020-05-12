cc6b0abac701081f1472d48b331842ffe3012a04
Max S-T <maxstantay@gmail.com>
2020-05-12 17:45:07 +0000

    it works

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,51 @@
 <script>
-	import Papa from "papaparse";
+	import axios from "axios";
 	import {onMount} from "svelte";
-
-	onMount(async () => {
-		Papa.parse('https://raw.githubusercontent.com/manfromth3m0oN/dice/master/test.csv', {
-			download: true,
-			complete: function (results) {
-				rawData = results.data;
-				listLength = results.data.length;
-			}
-		})
-		formatRawData();
-	})
-
 	let rawData;
 	let roll = 0;
-	let listLength;
-	let result;
-	let col1 = [];
-	let col2 = [];
-	let i;
+	let rolled;
+	let col1roll;
+	let col2roll;
 
-	function getRndInteger() {
-		roll = Math.floor(Math.random() * (listLength - 0 + 1) ) + 0;
-	}
+	onMount(async () => {
+		getJson().then(data => {
+			rawData = data
+		})
+	})
 
-	function formatRawData() {
-		for (i=0; i<listLength; i++) {
-			col1.push(rawData[i][0]);
-			col2.push(rawData[i][1]);
+	function getJson() {
+		return axios.get('https://raw.githubusercontent.com/manfromth3m0oN/dice/master/test.json')
+			.then((response) => {
+				console.log(response.data)
+				return response.data
+			})
+	};
+
+	function getRndInteger(max) {
+		return Math.floor(Math.random() * (max - 0 + 1) ) + 0;
+	};
+
+	function rollDie() {
+		if (Object.keys(rawData).length == 2) {
+			col1roll = getRndInteger(rawData['col1'].length - 1)
+			col2roll = getRndInteger(rawData['col2'].length - 1)
+		} else if (Object.keys(rawData).length == 1) {
+			coll1roll = getRndInteger(rawData['col1'].length)
 		}
-	}
-
-
+		rolled = true
+	};
 </script>
 
 <main>
-	The roll is {roll} <br/>
-	<button on:click={getRndInteger}>Roll the dice</button><br/>
-	{col1}
-	{col2}
+	<button on:click={rollDie}>Roll the dice</button><br/>
+	{#if col1roll > 0}
+		<p>Column 1:</p>{rawData['col1'][col1roll]}
+	{/if}
+
+	{#if col2roll > 0}
+		<p>Column 2:</p>{rawData['col2'][col2roll]}
+	{/if}
+
 </main>
 
 <style>
